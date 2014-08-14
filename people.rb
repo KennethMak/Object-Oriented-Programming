f2e0cf4abe8d614ef625ebc9bf4273d683100ff6
Kennethmak <Kenneth.mak@ryerson.ca>
2014-08-14 17:12:31 -0400

    classtime

--- people.rb
+++ people.rb
@@ -1,6 +1,31 @@
-class Student
+class Person
+	attr_accessor :name
+	def initialize(name)
+		@name = name
+	end
+	def greeting
+		puts "Hi, my name is #{name}"
+	end
+end
+
+
+class Student < Person
 	def learn
 		puts "I get it!"
 	end
 end
-		
+
+class Teacher < Person
+	def teach
+		puts "Everything in Ruby is an Object"
+	end
+end
+
+t = Teacher.new("Chris")
+t.greeting
+
+s = Student.new("Christina")
+s.greeting
+
+s.learn
+t.teach
